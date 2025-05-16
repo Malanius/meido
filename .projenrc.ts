72248e7b669a847ed50b2cc8d91b05986246795c
Michal Slota <malaniusprivierre@gmail.com>
2025-05-16 20:45:34 +0200

    chore: configure TS for local imports

--- .projenrc.ts
+++ .projenrc.ts
@@ -8,14 +8,22 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   name: 'waifu-bot',
   cdkVersion,
   cdkVersionPinning: true,
-  projenrcTs: true,
-
+  
   release: true,
   releaseTrigger: ReleaseTrigger.manual(),
   defaultReleaseBranch: 'main',
   github: false,
   packageManager: NodePackageManager.PNPM,
-
+  
+  projenrcTs: true,
+  tsconfig: {
+    compilerOptions: {
+      baseUrl: '.',
+      paths: {
+        '@/*': ['src/*'],
+      },
+    },
+  },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
