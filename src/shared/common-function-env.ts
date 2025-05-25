282d24ea2675c00228b725d1fd6f397da17e8215
Michal Slota <malaniusprivierre@gmail.com>
2025-05-25 18:20:59 +0200

    feat(shared): use shorter parameter cache for DEV env

--- src/shared/common-function-env.ts
+++ src/shared/common-function-env.ts
@@ -11,7 +11,10 @@ export const commonFunctionEnvironment = (
     POWERTOOLS_DEV: appStage === 'dev' ? 'true' : 'false',
     POWERTOOLS_LOG_LEVEL: appStage === 'prod' ? 'INFO' : 'DEBUG',
     POWERTOOLS_LOGGER_LOG_EVENT: appStage === 'prod' ? 'false' : 'true',
-    POWERTOOLS_PARAMETERS_MAX_AGE: `${60 * 60}`, // 1 hour
+    POWERTOOLS_PARAMETERS_MAX_AGE:
+      appStage === 'prod'
+        ? `${60 * 60}` // 1 Hour
+        : `${60 * 2}`, // 2 minutes
     POWERTOOLS_SERVICE_NAME: `${appName}::${appStage}::${serviceName}`,
   };
 };
