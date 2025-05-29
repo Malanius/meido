4e5b33be4d305dafa0a65c1d6fe3cdda31fa037b
Michal Slota <malaniusprivierre@gmail.com>
2025-05-29 15:00:00 +0200

    feat(core): send incoming command to event bus

--- src/core/interaction-handler/interaction-handler.lambda.ts
+++ src/core/interaction-handler/interaction-handler.lambda.ts
@@ -5,20 +5,32 @@ import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
 import { SecretsProvider } from '@aws-lambda-powertools/parameters/secrets';
 import { Tracer } from '@aws-lambda-powertools/tracer';
 import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
+import { EventBridgeClient, PutEventsCommand } from '@aws-sdk/client-eventbridge';
 import { SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
 import middy from '@middy/core';
 import type { APIGatewayProxyEventHeaders, APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { type APIInteraction, InteractionType } from 'discord-api-types/v10';
+import {
+  type APIApplicationCommandInteraction,
+  type APIInteraction,
+  InteractionResponseType,
+  InteractionType,
+} from 'discord-api-types/v10';
 import * as nacl from 'tweetnacl';
 
 const tracer = new Tracer();
 const logger = new Logger();
 const secretsClient = tracer.captureAWSv3Client(new SecretsManagerClient());
 const secretsProvider = new SecretsProvider({ awsSdkV3Client: secretsClient });
+const eventBridgeClient = tracer.captureAWSv3Client(new EventBridgeClient());
 
 const DISCORD_SECRET_NAME = env.DISCORD_SECRET_NAME;
 if (!DISCORD_SECRET_NAME) {
   throw new Error('DISCORD_SECRET_NAME environment variable is not set');
+}
+
+const EVENTS_BUS_NAME = env.EVENTS_BUS_NAME;
+if (!EVENTS_BUS_NAME) {
+  throw new Error('EVENTS_BUS_NAME environment variable is not set');
 }
 
 const isValidSignature = (publicKey: string, headers: APIGatewayProxyEventHeaders, rawBody?: string) => {
@@ -51,7 +63,7 @@ const lambdaHandler = async (event: APIGatewayProxyEventV2) => {
   const discordSecret = (await secretsProvider.get(DISCORD_SECRET_NAME, {
     transform: 'json',
   })) as DiscordSecret;
-  const publicKey = discordSecret?.publicKey;
+  const publicKey = discordSecret.publicKey;
   const headers = event.headers;
 
   if (!isValidSignature(publicKey, headers, event.body)) {
@@ -67,13 +79,42 @@ const lambdaHandler = async (event: APIGatewayProxyEventV2) => {
 
   if (body.type === InteractionType.Ping) {
     logger.debug('Received a PING interaction');
-    return response(200, { type: InteractionType.Ping });
+    return response(200, { type: InteractionResponseType.Pong });
   }
 
-  // TODO: Handle other interaction types
-  return response(501, { error: 'Not Implemented' });
+  if (body.type !== InteractionType.ApplicationCommand) {
+    logger.warn('Received an unsupported interaction type', { type: body.type });
+    return response(400, { error: 'Unsupported interaction type' });
+  }
+
+  const command = body as APIApplicationCommandInteraction;
+  try {
+    await sendEvent(command);
+  } catch (error) {
+    logger.error('Failed to send event', { error });
+    return response(500, { error: 'Failed to send event' });
+  }
+
+  return response(200, { type: InteractionResponseType.DeferredChannelMessageWithSource });
 };
 
 export const handler = middy(lambdaHandler)
   .use(captureLambdaHandler(tracer))
   .use(injectLambdaContext(logger, { clearState: true }));
+
+const sendEvent = async (event: APIApplicationCommandInteraction) => {
+  const command = new PutEventsCommand({
+    Entries: [
+      {
+        EventBusName: EVENTS_BUS_NAME,
+        Source: 'dev.malanius.waifu-bot',
+        DetailType: event.data.name,
+        Time: new Date(),
+        Detail: JSON.stringify(event),
+      },
+    ],
+  });
+  logger.debug('Sending event', { command });
+  const response = await eventBridgeClient.send(command);
+  logger.debug('Event sent', { response });
+};
