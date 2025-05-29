02a9df8e3b78d07974c2934b3dc9716adbdb411e
Michal Slota <malaniusprivierre@gmail.com>
2025-05-29 21:05:24 +0200

    feat(ping): create ping handler function

---
+++ src/ping/ping.lambda.ts
@@ -0,0 +1,31 @@
+import { env } from 'node:process';
+import type { DiscordSecret } from '@/types';
+import { Logger } from '@aws-lambda-powertools/logger';
+import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
+import { SecretsProvider } from '@aws-lambda-powertools/parameters/secrets';
+import { Tracer } from '@aws-lambda-powertools/tracer';
+import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
+import { SecretsManagerClient } from '@aws-sdk/client-secrets-manager';
+import middy from '@middy/core';
+import type { EventBridgeEvent } from 'aws-lambda';
+import type { APIApplicationCommand } from 'discord-api-types/v10';
+
+const tracer = new Tracer();
+const logger = new Logger();
+const secretsClient = tracer.captureAWSv3Client(new SecretsManagerClient());
+const secretsProvider = new SecretsProvider({ awsSdkV3Client: secretsClient });
+
+const DISCORD_SECRET_NAME = env.DISCORD_SECRET_NAME;
+if (!DISCORD_SECRET_NAME) {
+  throw new Error('DISCORD_SECRET_NAME environment variable is not set');
+}
+
+const lambdaHandler = async (event: EventBridgeEvent<'ping', APIApplicationCommand>) => {
+  const discordSecret = (await secretsProvider.get(DISCORD_SECRET_NAME, {
+    transform: 'json',
+  })) as DiscordSecret;
+};
+
+export const handler = middy(lambdaHandler)
+  .use(captureLambdaHandler(tracer))
+  .use(injectLambdaContext(logger, { clearState: true }));
--- src/ping/ping.stack.ts
+++ src/ping/ping.stack.ts
@@ -1,14 +1,61 @@
+import { EVENTS_SOURCE } from '@/shared/constants';
 import { DiscordSlashCommand } from '@/shared/discord-slash-command/discord-slash-command';
+import { commonFunctionEnvironment } from '@/shared/functions/common-env';
+import { commonFunctionProps } from '@/shared/functions/common-props';
 import type { AppInfo } from '@/types';
 import type { StackProps } from 'aws-cdk-lib';
-import { Aspects, Stack, Tag } from 'aws-cdk-lib';
+import { Aspects, Duration, RemovalPolicy, Stack, Tag } from 'aws-cdk-lib';
+import { EventBus, Rule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import type { Construct } from 'constructs';
+import { PingFunction } from './ping-function';
+
+const MODULE = 'ping';
 
 export interface PingProps extends StackProps, AppInfo {}
 
 export class Ping extends Stack {
   constructor(scope: Construct, id: string, props: PingProps) {
     super(scope, id, props);
+
+    const { appName, appStage } = props;
+    const eventsBusName = `${appName}-${appStage}`;
+
+    const eventBus = EventBus.fromEventBusName(this, 'EventsBus', eventsBusName);
+    const discordSecrets = Secret.fromSecretNameV2(this, 'DiscordSecrets', `/${appName}/${appStage}/discord`);
+
+    const logGroup = new LogGroup(this, 'LogGroup', {
+      logGroupName: `/${appName}/${appStage}/${MODULE}`,
+      retention: RetentionDays.ONE_WEEK,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const pingHandler = new PingFunction(this, 'PingHandler', {
+      ...commonFunctionProps,
+      environment: {
+        ...commonFunctionEnvironment(props, MODULE),
+        DISCORD_SECRET_NAME: discordSecrets.secretName,
+      },
+      logGroup,
+    });
+
+    discordSecrets.grantRead(pingHandler);
+
+    new Rule(this, 'PingRule', {
+      eventBus,
+      eventPattern: {
+        source: [EVENTS_SOURCE],
+        detailType: [MODULE],
+      },
+      targets: [
+        new LambdaFunction(pingHandler, {
+          maxEventAge: Duration.minutes(1),
+          retryAttempts: 2,
+        }),
+      ],
+    });
 
     new DiscordSlashCommand(this, 'Ping', {
       ...props,
