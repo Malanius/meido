d8fd0a7f8d5390e26d18076ce366ac529fe9ef51
Michal Slota <malaniusprivierre@gmail.com>
2025-06-05 22:32:46 +0200

    feat(tadaima): use new event structure

--- src/tadaima/tadaima.lambda.ts
+++ src/tadaima/tadaima.lambda.ts
@@ -1,27 +1,25 @@
 import { DiscordApiClient } from '@/shared/discord-api-client';
+import type { MeidoInteraction } from '@/types';
 import { Logger } from '@aws-lambda-powertools/logger';
 import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
 import { Tracer } from '@aws-lambda-powertools/tracer';
 import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
 import middy from '@middy/core';
 import type { EventBridgeEvent } from 'aws-lambda';
-import type { APIApplicationCommandInteraction } from 'discord-api-types/v10';
 
 const tracer = new Tracer();
 const logger = new Logger();
 
-const lambdaHandler = async (event: EventBridgeEvent<'tadaima', APIApplicationCommandInteraction>) => {
-  const discordApiClient = new DiscordApiClient(event.detail.application_id, event.detail.guild_id);
+const lambdaHandler = async (event: EventBridgeEvent<'tadaima', MeidoInteraction>) => {
+  const { application_id, guild_id, token } = event.detail.command;
+  const discordApiClient = new DiscordApiClient(application_id, guild_id, token);
 
   const eventTimestamp = new Date(event.time);
   const now = new Date();
   const diff = now.getTime() - eventTimestamp.getTime();
   const diffInSeconds = diff / 1000;
 
-  await discordApiClient.sendFollowupMessage(
-    event.detail.token,
-    `🏓 Pong! Serverless took ~${diffInSeconds}s to respond.`
-  );
+  await discordApiClient.sendFollowupMessage(token, `🏓 Pong! Serverless took ~${diffInSeconds}s to respond.`);
 };
 
 export const handler = middy(lambdaHandler)
