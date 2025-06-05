9ea8621a9351a8f643b7469f2d22c480420a8abe
Michal Slota <malaniusprivierre@gmail.com>
2025-06-05 23:23:21 +0200

    feat(tadaima): send proper welcome messages

--- src/tadaima/tadaima.lambda.ts
+++ src/tadaima/tadaima.lambda.ts
@@ -10,19 +10,62 @@ import type { EventBridgeEvent } from 'aws-lambda';
 const tracer = new Tracer();
 const logger = new Logger();
 
-let coldStart = true;
+const JP_PROBABILITY = 0.15;
+
+const DICTIONARY = {
+  en: {
+    welcome: 'Welcome home',
+    master: 'Master',
+    sorry: 'Sorry',
+  },
+  ja: {
+    welcome: 'おかえりなさい',
+    master: 'ご主人様',
+    sorry: 'ごめんなさい',
+  },
+};
+
+const getWelcomeMessage = (isMaster: boolean) => {
+  const lang = Math.random() < JP_PROBABILITY ? 'ja' : 'en';
+  if (isMaster) {
+    return `**${DICTIONARY[lang].welcome}**, ${DICTIONARY[lang].master}! :blush:`;
+  }
+
+  return `${DICTIONARY[lang].welcome}!`;
+};
+
+const getColdStartMessage = (endpointColdStart: boolean, handlerColdStart: boolean) => {
+  const lang = Math.random() < JP_PROBABILITY ? 'ja' : 'en';
+  if (endpointColdStart && handlerColdStart) {
+    return `I've been very cold :cold_face:, ${DICTIONARY[lang].sorry}! :woman_bowing:`;
+  }
+
+  if (endpointColdStart || handlerColdStart) {
+    return `I've been slightly cold :snowflake:, ${DICTIONARY[lang].sorry}! :woman_bowing:`;
+  }
+
+  return '';
+};
+
+let handlerColdStart = true;
 
 const lambdaHandler = async (event: EventBridgeEvent<'tadaima', MeidoInteraction>) => {
-  coldStart = false;
   const { application_id, guild_id, token } = event.detail.command;
   const discordApiClient = new DiscordApiClient(application_id, guild_id, token);
+
+  const welcomeMessage = getWelcomeMessage(event.detail.invokedByMaster);
+  const coldStartMessage = getColdStartMessage(event.detail.endpointColdStart, handlerColdStart);
 
   const eventTimestamp = new Date(event.time);
   const now = new Date();
   const diff = now.getTime() - eventTimestamp.getTime();
   const diffInSeconds = diff / 1000;
+  const timeToRespond = `It took me ~${diffInSeconds}s to respond...`;
 
-  await discordApiClient.sendFollowupMessage(token, `🏓 Pong! Serverless took ~${diffInSeconds}s to respond.`);
+  const message = `${welcomeMessage} ${timeToRespond} ${coldStartMessage}`;
+
+  await discordApiClient.sendFollowupMessage(token, message);
+  handlerColdStart = false;
 };
 
 export const handler = middy(lambdaHandler)
