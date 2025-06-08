e160422b3f5702d1f5d0ade944ed0c1fd0e678dc
Michal Slota <malaniusprivierre@gmail.com>
2025-06-08 20:33:26 +0200

    feat(core): forward DDB stream events to event bus

--- src/core/database/dynamo-bridge.lambda.ts
+++ src/core/database/dynamo-bridge.lambda.ts
@@ -1,18 +1,57 @@
+import { env } from 'node:process';
+import { EventsSource } from '@/shared/event-source';
 import { BatchProcessor, EventType, processPartialResponse } from '@aws-lambda-powertools/batch';
 import { Logger } from '@aws-lambda-powertools/logger';
 import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
 import { Tracer } from '@aws-lambda-powertools/tracer';
 import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
+import { EventBridgeClient, PutEventsCommand } from '@aws-sdk/client-eventbridge';
 import middy from '@middy/core';
-import type { DynamoDBRecord, DynamoDBStreamHandler } from 'aws-lambda';
+import type { AttributeValue, DynamoDBRecord, DynamoDBStreamHandler } from 'aws-lambda';
 
 const tracer = new Tracer();
 const logger = new Logger();
+const eventBridgeClient = tracer.captureAWSv3Client(new EventBridgeClient());
 const processor = new BatchProcessor(EventType.DynamoDBStreams);
 
-const recordHandler = async (record: DynamoDBRecord): Promise<void> => {
-  logger.info('Processing record', { record });
-  // TODO: Implement record processing
+const EVENTS_BUS_NAME = env.EVENTS_BUS_NAME;
+if (!EVENTS_BUS_NAME) {
+  throw new Error('EVENTS_BUS_NAME environment variable is not set');
+}
+
+interface ValidStreamRecord extends DynamoDBRecord {
+  eventName: 'INSERT' | 'MODIFY' | 'REMOVE';
+  dynamodb: {
+    // Key schema is quaranteed by our table definition
+    Keys: {
+      pk: { S: string };
+      sk: { S: string };
+    };
+    NewImage?: Record<string, AttributeValue>;
+    OldImage?: Record<string, AttributeValue>;
+  };
+}
+
+type AssertValidRecord = (record: DynamoDBRecord) => asserts record is ValidStreamRecord;
+const assertValidRecord: AssertValidRecord = (record) => {
+  if (
+    !record.eventName ||
+    !record.dynamodb ||
+    !record.dynamodb.Keys ||
+    !record.dynamodb.Keys.pk ||
+    !record.dynamodb.Keys.sk ||
+    typeof record.dynamodb.Keys.pk.S !== 'string' ||
+    typeof record.dynamodb.Keys.sk.S !== 'string' ||
+    (!record.dynamodb.NewImage && !record.dynamodb.OldImage)
+  ) {
+    throw new Error('Invalid DynamoDB stream record');
+  }
+};
+
+const semanticAction = {
+  INSERT: 'Created',
+  MODIFY: 'Updated',
+  REMOVE: 'Deleted',
 };
 
 const lambdaHandler: DynamoDBStreamHandler = async (event, context) =>
@@ -23,3 +62,38 @@ const lambdaHandler: DynamoDBStreamHandler = async (event, context) =>
 export const handler = middy(lambdaHandler)
   .use(captureLambdaHandler(tracer))
   .use(injectLambdaContext(logger, { clearState: true }));
+
+const recordHandler = async (record: DynamoDBRecord): Promise<void> => {
+  logger.info('Processing record', { record });
+  assertValidRecord(record);
+  const action = semanticAction[record.eventName];
+  const pk = record.dynamodb.Keys.pk.S;
+  const [module, entity, _entityId] = pk.split('#');
+  const detailType = `${module}.${entity}.${action}`;
+  await sendEvent(detailType, record.dynamodb.NewImage, record.dynamodb.OldImage);
+};
+
+const sendEvent = async (
+  detailType: string,
+  newImage?: Record<string, AttributeValue>,
+  oldImage?: Record<string, AttributeValue>
+) => {
+  const payload = {
+    newImage,
+    oldImage,
+  };
+  const event = new PutEventsCommand({
+    Entries: [
+      {
+        EventBusName: EVENTS_BUS_NAME,
+        Source: EventsSource.Database,
+        DetailType: detailType,
+        Time: new Date(),
+        Detail: JSON.stringify(payload),
+      },
+    ],
+  });
+  logger.debug('Sending event', { event });
+  const response = await eventBridgeClient.send(event);
+  logger.debug('Event sent', { response });
+};
