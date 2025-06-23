8a85a9eb4696c6aeae31334e69e88c00a72600d3
Michal Slota <malaniusprivierre@gmail.com>
2025-06-23 21:05:43 +0200

    feat(journal): implement journal subscription

--- src/journal/subscription-manager/subscription-manager.lambda.ts
+++ src/journal/subscription-manager/subscription-manager.lambda.ts
@@ -6,7 +6,7 @@ import { Tracer } from '@aws-lambda-powertools/tracer';
 import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
 import middy from '@middy/core';
 import type { EventBridgeEvent } from 'aws-lambda';
-import { getSubscriptionInfo } from './commands/info';
+import { getSubscriptionInfo, subscribe } from './commands';
 
 const tracer = new Tracer();
 const logger = new Logger();
@@ -33,7 +33,7 @@ const lambdaHandler = async (event: EventBridgeEvent<'journal', MeidoInteraction
       case 'info':
         return await getSubscriptionInfo(event.detail.command);
       case 'subscribe':
-      // TODO: Implement subscribe logic
+        return await subscribe(event.detail.command);
       case 'unsubscribe':
         // TODO: Implement unsubscribe logic
         return `ごめんなさい！ Master-sama didn't trained me yet how to handle \`${action}\`. :woman_bowing:`;
